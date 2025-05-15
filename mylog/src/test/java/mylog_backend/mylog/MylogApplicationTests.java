ef1be090e91daf3883973d593a3c39d91b2ebaef
daiseek <dnjsgml0578@gmail.com>
2025-05-16 00:04:16 +0900

    chore/#3 : 도커 세팅 - Dockerfile, docker-compose.yml 작성 - .env 파일 세팅후 .gitignore에 명시 - 테스트 코드용으로 h2 인메모리 방식 사용 - 서버용, 테스트용 application 파일 구성

--- mylog/src/test/java/mylog_backend/mylog/MylogApplicationTests.java
+++ mylog/src/test/java/mylog_backend/mylog/MylogApplicationTests.java
@@ -2,8 +2,10 @@ package mylog_backend.mylog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MylogApplicationTests {
 
 	@Test
