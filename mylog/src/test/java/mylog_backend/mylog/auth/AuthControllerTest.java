806fcbcc6c12dedf965c2a181fdd90e51090a7d9
daiseek <dnjsgml0578@gmail.com>
2025-06-02 17:17:15 +0900

    dev-ops/#48 : CI/CD 설정 - GitHub Actions로 CI 설정 - 추후 Amazon EC2로 CD하도록 설정 필요 - 로컬용 docker-compose.override.yml 설정

--- mylog/src/test/java/mylog_backend/mylog/auth/AuthControllerTest.java
+++ mylog/src/test/java/mylog_backend/mylog/auth/AuthControllerTest.java
@@ -5,6 +5,7 @@ import mylog_backend.mylog.config.EmbeddedRedisConfig;
 import mylog_backend.mylog.user.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -22,6 +23,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
+@Tag("external-integration") // CI에서 빌드시 해당 파일 제외
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
--- mylog/src/test/java/mylog_backend/mylog/auth/CreateJwtTest.java
+++ mylog/src/test/java/mylog_backend/mylog/auth/CreateJwtTest.java
@@ -1,5 +1,6 @@
 package mylog_backend.mylog.auth;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,6 +8,8 @@ import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+
+@Tag("external-integration") // CI에서 빌드시 해당 파일 제외
 @SpringBootTest
 @ActiveProfiles("test")
 class CreateJwtTest {
