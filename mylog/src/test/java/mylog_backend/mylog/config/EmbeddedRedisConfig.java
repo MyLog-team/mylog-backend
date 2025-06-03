334addbe932ad688f985f47cb242ac57b8fb1348
daiseek <dnjsgml0578@gmail.com>
2025-06-03 12:23:12 +0900

    dev-ops/#48 : CI/CD 설정 - GitHub Actions로 CI 설정 - 추후 Amazon EC2로 CD하도록 설정 필요 - 로컬용 docker-compose.override.yml 설정 - 로그아웃 테스트를 위해 보호된 리소스에 접근하는 로직 롤백 - JWT필터에서 /auth보다 구체적으로 경로 지정 -     @DynamicPropertySource로 테스트용 임베디드 레디스 설정

--- mylog/src/test/java/mylog_backend/mylog/config/EmbeddedRedisConfig.java
+++ mylog/src/test/java/mylog_backend/mylog/config/EmbeddedRedisConfig.java
@@ -3,6 +3,8 @@ package mylog_backend.mylog.config;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import redis.embedded.RedisServer;
 
 import java.io.IOException;
@@ -10,11 +12,12 @@ import java.io.IOException;
 @TestConfiguration
 public class EmbeddedRedisConfig {
 
+    private static final int REDIS_PORT = 6380;
     private RedisServer redisServer;
 
     @PostConstruct
     public void startRedis() throws IOException {
-        redisServer = new RedisServer(6380);
+        redisServer = new RedisServer(REDIS_PORT);
         redisServer.start();
     }
 
@@ -23,6 +26,17 @@ public class EmbeddedRedisConfig {
         if (redisServer != null) {
             redisServer.stop();
         }
+    }
+
+
+    /**
+     * Redis의 포트, 호스트 값을 덮어씌움
+     * @param registry
+     */
+    @DynamicPropertySource
+    public static void setRedisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.redis.port", () -> String.valueOf(REDIS_PORT));
+        registry.add("spring.redis.host", () -> "localhost");
     }
 }
 
